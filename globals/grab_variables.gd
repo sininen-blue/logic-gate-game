963f216fb6d1d2aa95273ac4033105fcce329255
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-08-20 20:22:10 +0800

    feature: finished outputs for connection

--- globals/grab_variables.gd
+++ globals/grab_variables.gd
@@ -20,8 +20,8 @@ var state = IDLE
 
 
 # TODO:
-#current issue is that you can have multiple connections with the same start and stop
-#(done) also need to recheck the cleanup of values since sometimes things tend to jump around
+# current issue is that you can have multiple connections with the same start and stop
+# a way to delete connections
 
 func _process(_delta: float) -> void:
 	handle_connections()
@@ -55,7 +55,9 @@ func _process(_delta: float) -> void:
 					
 					start_gate = null
 					end_gate = null
+					potential_close = null
 					current_connection = null
+					con_type = Con_Type.NONE
 					state = IDLE
 				else:
 					remove_child(current_connection)
@@ -74,8 +76,10 @@ func _on_gate_unhover(source : Area2D):
 		current_gate = null
 
 
-var connections : Array
 
+var connections : Array
+enum Con_Type {NONE, INPUT, OUTPUT}
+var con_type = Con_Type.NONE
 var start_gate : Area2D
 var end_gate : Area2D
 var current_connection : Line2D
@@ -90,23 +94,37 @@ func handle_connections():
 		connection[0].set_point_position(0 ,connection[1].global_position)
 		connection[0].set_point_position(1 ,connection[2].global_position)
 
-
+# TODO add a check to make sure inputs only go to outputs and vice versa
 func _on_input_hover(source : Area2D):
+	if current_connection == null:
+		var line : Line2D = Line2D.new()
+		line.add_point(source.global_position)
+		
+		start_gate = source
+		current_connection = line
+		con_type = Con_Type.INPUT
+	else:
+		if current_connection.get_point_position(0) != source.global_position:
+			if con_type == Con_Type.OUTPUT:
+				potential_close = source
+
+func _on_output_hover(source : Area2D):
 	if current_connection == null:
 		var line : Line2D = Line2D.new()
 		start_gate = source
 		line.add_point(start_gate.global_position)
 		current_connection = line
+		con_type = Con_Type.OUTPUT
 	else:
-		# check if the hover is from a separate input point
-		# need to add extra check to only get output points
-		
-		# NOTE: THIS DOESN'T WORK YET
-		# IT ONLY CONNECTS TO OTHER INPUT AREAS
 		if current_connection.get_point_position(0) != source.global_position:
-			potential_close = source
+			if con_type == Con_Type.INPUT:
+				potential_close = source
 
 
 func _on_input_unhover(source : Area2D):
+	if current_connection != null and state != CREATING_CONNECTION:
+		current_connection = null
+
+func _on_output_unhover(source : Area2D):
 	if current_connection != null and state != CREATING_CONNECTION:
 		current_connection = null
